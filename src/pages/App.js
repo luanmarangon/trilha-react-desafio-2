6e28f25cdb94b83d50d483b641bee7bb33069015
luanmarangon-egestora <luan.marangon@egestora.com.br>
2024-10-13 11:57:58 -0300

    conclusao desafio

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,9 +34,8 @@ function App() {
   }
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
     // utilizar filter.
+    setRepos(prevRepos => prevRepos.filter(repo => repo.id !== id));
   }
 
 
